7bbfa07bca4dc49a737222e6884c17b8da469fc7
Yakumaa <maharjanshrish8@gmail.com>
2024-03-31 22:15:46 +0545

    3.5: Add POST endpoint for creating new persons

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { 
     "id": 1,
@@ -54,6 +56,30 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(p => p.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const randomId = Math.floor(Math.random() * 1000)
+  return randomId
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
