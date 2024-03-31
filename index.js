411bef3b907935f4280757eee47b22d266de87b1
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-03-31 07:59:51 +0545

    3.2: Refactor routes in index.js

--- index.js
+++ index.js
@@ -24,12 +24,16 @@ let persons = [
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World!</h1>')
+// })
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/api/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people <br/> ${new Date()} </p>`)
 })
 
 const PORT = 3001
