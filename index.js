da315569236ab40a7a665c03dd3258668cb2b2e9
Yakumaa <maharjanshrish8@gmail.com>
2024-04-05 13:46:28 +0545

    3.13-3.14: Add .env file and update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -21,37 +23,38 @@ const requestLogger = (request, response, next) => {
 app.use(requestLogger)
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
+  // { 
+  //   "id": 1,
+  //   "name": "Arto Hellas", 
+  //   "number": "040-123456"
+  // },
+  // { 
+  //   "id": 2,
+  //   "name": "Ada Lovelace", 
+  //   "number": "39-44-5323523"
+  // },
+  // { 
+  //   "id": 3,
+  //   "name": "Dan Abramov", 
+  //   "number": "12-43-234345"
+  // },
+  // { 
+  //   "id": 4,
+  //   "name": "Mary Poppendieck", 
+  //   "number": "39-23-6423122"
+  // }
 ]
 
 // app.get('/', (request, response) => {
 //   response.send('<h1>Hello World!</h1>')
 // })
 
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')) 
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/info', (request, response) => {
@@ -60,16 +63,9 @@ app.get('/api/info', (request, response) => {
 // app.use(requestLogger)
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  // console.log(person)
-  const person = persons.find(p => p.id === id)
-
-  if (person){
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  }
-  else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -79,10 +75,10 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-  const randomId = Math.floor(Math.random() * 1000)
-  return randomId
-}
+// const generateId = () => {
+//   const randomId = Math.floor(Math.random() * 1000)
+//   return randomId
+// }
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -103,14 +99,15 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
+    // id: generateId(),
+  })
 
-  persons = persons.concat(person)
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
@@ -118,7 +115,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
